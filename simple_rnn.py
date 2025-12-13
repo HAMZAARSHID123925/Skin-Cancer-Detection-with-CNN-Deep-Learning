259454fc4f28c55eaa343f7797e3cc0224ba672a
HAMZAARSHID123925 <hamzahayat0310@gmail.com>
2025-12-13 11:39:46 -0800

    Remove matplotlib import from simple_rnn.py

--- simple_rnn.py
+++ simple_rnn.py
@@ -9,7 +9,6 @@ Original file is located at
 
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 
